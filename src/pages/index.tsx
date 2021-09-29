426f41a528eb3c3e73343034eed3e158e61ae24e
Lance Hasson <lance.hasson@gmail.com>
2021-09-28 17:13:27 -0700

    Add fontawesome, types and update index to show it loading correctly

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,14 @@
+import { faExclamation } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 export default function Home() {
   return (
     <div className="container flex items-center p-4 mx-auto min-h-screen justify-center">
       <main>
         <h1 className="font-mono text-xl code">
-          Welcome to <span className="text-purple-700">Nextjs</span>, <span className="text-indigo-700">TailwindCSS</span> and <span className="text-gray-700">TypeScript</span>
+          Welcome to <span className="text-purple-700">Nextjs</span>, <span className="text-indigo-700">TailwindCSS</span> and <span className="text-gray-700">TypeScript</span> and Font Awesome<span className="text-purple-500">
+            <FontAwesomeIcon icon={faExclamation} />
+          </span>
         </h1>
       </main>
     </div>
